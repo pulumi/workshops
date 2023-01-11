c6597b14770f35dd7a506c368ea32427b7c9ec6d
Tushar Shah <tushar.shah1@gmail.com>
2023-01-11 15:54:39 -0500

    added outputs for blob storage container

---
+++ archive/azure-intro/csharp/01-iac/code/04-updating-your-infrastructure/step5.cs
@@ -0,0 +1,43 @@
+using Pulumi;
+using Azure = Pulumi.AzureNative;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+class MyStack : Stack
+{
+    public MyStack()
+    {
+        // Add your resources here
+        // Create an Azure Resource Group
+        var resourceGroup = new ResourceGroup("myrgroup");
+        // Export the resource group name
+        this.resourcegroup_name = resourceGroup.Name;
+
+        // Create an Azure resource (Storage Account)
+        var storageAccount = new StorageAccount("mystorageact", new StorageAccountArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Sku = new SkuArgs
+            {
+                Name = SkuName.Standard_LRS
+            },
+            Kind = Kind.StorageV2
+        });
+        // Export the Storage Account Name
+        this.AccountName =  storageAccount.Name;
+
+        // Create a Blob Container
+        var blobContainer = new BlobContainer("mycontainer", new BlobContainerArgs
+        {
+            ResourceGroupName= resourceGroup.Name,
+            AccountName = storageAccount.Name,   
+            ContainerName = "files",
+        });
+        // Export the BlobContainer Name
+        this.ContainerName = blobContainer.Name;
+    }
+    // Add Outputs here
+    [Output("resourcegroup_name")] public Output<string> resourcegroup_name { get; set; }
+    [Output("AccountName")] public Output<string> AccountName { get; set; }
+    [Output("ContainerName")] public Output<string> ContainerName { get; set; }
+}
