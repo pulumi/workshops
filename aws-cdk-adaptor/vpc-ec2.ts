c85cd159a377dc9031ba4855d8f2eb08e8d3efdc
Torian Crane <toriancrane@gmail.com>
2024-12-09 17:50:58 +0100

    Update vpc-ec2.ts

--- aws-cdk-adaptor/vpc-ec2.ts
+++ aws-cdk-adaptor/vpc-ec2.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from '@pulumi/aws';
 import * as pulumicdk from '@pulumi/cdk';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as cdk_ec2 from 'aws-cdk-lib/aws-ec2';
 
 const app = new pulumicdk.App('app', (scope: pulumicdk.App) => {
     
@@ -9,24 +9,24 @@ const app = new pulumicdk.App('app', (scope: pulumicdk.App) => {
     
     const stack = new pulumicdk.Stack(scope, 'vpc-stack');
 
-    const vpc = new ec2.Vpc(stack, 'MainVPC', {
+    const vpc = new cdk_ec2.Vpc(stack, 'MainVPC', {
         maxAzs: 2,
         natGateways: 1,
     });
 
-    const securityGroup = new ec2.SecurityGroup(stack, 'WebServerSG', {
+    const securityGroup = new cdk_ec2.SecurityGroup(stack, 'WebServerSG', {
         vpc,
-        description: 'Allow HTTP access to EC2 instances',
+        description: 'Allow HTTP access to cdk_ec2 instances',
         allowAllOutbound: true,
     });
 
     securityGroup.addIngressRule(
-        ec2.Peer.anyIpv4(),
-        ec2.Port.tcp(80),
+        cdk_ec2.Peer.anyIpv4(),
+        cdk_ec2.Port.tcp(80),
         'Allow HTTP access from anywhere'
     );
 
-    //// Pulumi-Created Resources ////
+    //// Pulumi Created Resources ////
 
     const userData = `
     #!/bin/bash
@@ -36,13 +36,13 @@ const app = new pulumicdk.App('app', (scope: pulumicdk.App) => {
     sudo systemctl enable nginx
     sudo systemctl start nginx`;
 
-    const ami = aws.ec2.getAmiOutput({
+    const ami = aws.cdk_ec2.getAmiOutput({
         filters: [{ name: "name", values: ["amzn2-ami-hvm-*"] }],
         owners: ["amazon"],
         mostRecent: true,
     });
 
-    const instance = new aws.ec2.Instance("webserver-www", {
+    const instance = new aws.cdk_ec2.Instance("webserver-www", {
         instanceType: "t2.micro",
         ami: ami.id,
         userData: userData,
