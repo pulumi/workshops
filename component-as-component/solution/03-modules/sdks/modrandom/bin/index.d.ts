5aac0e2aab1ed59c303513b11f3ab455d2b642f1
Rob Smith <smith.rob.s@gmail.com>
2025-05-30 17:10:22 -0400

    Add simpler comp-as-comp (#171)
    * Add Designing Reusable Infrastructure as Code

---
+++ component-as-component/solution/03-modules/sdks/modrandom/bin/index.d.ts
@@ -0,0 +1,6 @@
+export { ModuleArgs } from "./module";
+export type Module = import("./module").Module;
+export declare const Module: typeof import("./module").Module;
+export { ProviderArgs } from "./provider";
+export type Provider = import("./provider").Provider;
+export declare const Provider: typeof import("./provider").Provider;
