b47c0e153cdc2f3deed57305b86b7980b4aa9bd3
Rob Smith <smith.rob.s@gmail.com>
2025-08-06 11:53:27 -0400

    Add golden component example (#178)

---
+++ golden-paths-infrastructure-components-and-templates/solution/01-component-microservice/microserviceComponent.ts
@@ -0,0 +1,77 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
+
+export class MicroserviceComponent extends pulumi.ComponentResource {
+    lb: awsx.lb.ApplicationLoadBalancer;
+    cluster: aws.ecs.Cluster;
+
+    public readonly publicUrl: pulumi.Output<string>;
+
+    constructor(name: string, args: MicroserviceComponentArgs, opts?: pulumi.ComponentResourceOptions) {
+        super("component-microservice:index:MicroserviceComponent", name, args, opts);
+
+        // Create an ECR repository for pushing.
+        const ecrRepository = new awsx.ecr.Repository("repo", {
+            forceDelete: true, // This will delete the repository and all images when the stack is destroyed.
+        }, { parent: this });
+
+        // Build and push an image to ECR
+        const image = new awsx.ecr.Image("image", {
+            repositoryUrl: ecrRepository.url,
+            context: args.appPath,
+            platform: "linux/amd64",
+        }, { parent: this });
+
+        this.lb = new awsx.lb.ApplicationLoadBalancer("lb", {
+            defaultTargetGroup: { port: args.port }
+        }, { parent: this });
+        this.cluster = new aws.ecs.Cluster("cluster", {}, { parent: this });
+
+        const service = new awsx.ecs.FargateService("service", {
+            cluster: this.cluster.arn,
+            assignPublicIp: true,
+            desiredCount: 2,
+            taskDefinitionArgs: {
+                container: {
+                    name: "my-service",
+                    image: image.imageUri,
+                    cpu: args.cpu || 256, // Default to 0.25 vCPU,
+                    memory: args.memory || 512, // Default to 0.5 GB,
+                    essential: true,
+                    portMappings: [
+                        {
+                            containerPort: args.port,
+                            targetGroup: this.lb.defaultTargetGroup,
+                        },
+                    ],
+                },
+            },
+        }, { parent: this });
+
+        this.publicUrl = pulumi.interpolate`http://${this.lb.loadBalancer.dnsName}`;
+
+        this.registerOutputs({
+            publicUrl: this.publicUrl
+        });
+    }
+}
+
+export interface MicroserviceComponentArgs {
+    /**
+     * Path to a dockerized application to serve. Directory must contain a dockerfile
+     */
+    appPath: pulumi.Input<string>;
+    /**
+     * Port the application exposes
+     */
+    port: pulumi.Input<number>;
+    /**
+     * (Optional) CPU capacity. Defaults to 256 (i.e. 0.25 vCPU).
+     */
+    cpu?: pulumi.Input<number>;
+    /**
+     * (Optional) Memory capacity. Defaults to 512 (i.e. 0.5GB).
+     */
+    memory?: pulumi.Input<number>;
+}
